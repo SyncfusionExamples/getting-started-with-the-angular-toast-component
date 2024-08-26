f73b7a377e30d51ceb17f9122c442b506de15de5
VenkateshwaranSaravanakumar <107187632+VenkateshwaranSaravanakumar@users.noreply.github.com>
2024-08-26 18:23:32 +0530

    Updated the sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,25 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToastModule } from '@syncfusion/ej2-angular-notifications'
+import { ToastModule, ToastComponent } from '@syncfusion/ej2-angular-notifications'
 import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToastModule, ButtonModule ],
+  imports: [RouterOutlet, ToastModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  @ViewChild('toastObj') public toastObj: any;
+  @ViewChild('toastObj') public toastObj: ToastComponent;
+
   public position = { X: 'Right', Y: 'Top' };
 
-  onCreate(args: any) {
+  showToast(args: any) {
     this.toastObj.show();
   }
 
-  showClick(args: any) {
-    this.toastObj.show();
-  }
-
-  hideClick(args: any) {
+  hideToast(args: any) {
     this.toastObj.hide('All');
   }
-
-  
 }
